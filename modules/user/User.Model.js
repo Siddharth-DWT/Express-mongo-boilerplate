a0b540c96da2ebd6843882ce4dbe6c1357e4eacc
Aman Jat <aman@discoverwebtech.com>
2022-03-04 16:09:39 +0530

    format - prettier

--- modules/user/User.Model.js
+++ modules/user/User.Model.js
@@ -1,40 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
 
-    userType: {
-        type: String
-    },
-    gender:{
-        type: String,
-        required: true
-    },
-    age:{
-        type: String,
-        required: true
-    },
-    phone:{
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    resetLink: {
-        type: String,
-    },
-    img: {
-        contentType: String
-    }
+  userType: {
+    type: String,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  resetLink: {
+    type: String,
+  },
+  img: {
+    contentType: String,
+  },
 });
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
